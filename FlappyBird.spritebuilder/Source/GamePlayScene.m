2c64ef62e7a9708e79ba806e554e9b5f9987d889
poofytoo <victorhung92@gmail.com>
2015-02-06 23:49:22 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -6,7 +6,8 @@
 
 - (void)initialize
 {
-    // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
@@ -14,6 +15,8 @@
     // put update code here
 }
 
-// put new methods here
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    [character flap];
+}
 
 @end
